393a082aab44b3df242651b270f9b04220a66bed
hunjixin <1084400399@qq.com>
2025-06-08 14:37:06 +0000

    feat: add default function  for build and more option set

--- deepseek-api/src/client_builder.rs
+++ deepseek-api/src/client_builder.rs
@@ -1,4 +1,5 @@
 use anyhow::{Ok, Result};
+use std::env;
 cfg_if::cfg_if! {
     if #[cfg(feature = "is_sync")] {
         use reqwest::blocking::ClientBuilder as ReqwestClientBuilder;
@@ -19,7 +20,7 @@ use std::time::Duration;
 ///
 /// ```ignore
 /// let client = DeepSeekClientBuilder::new("your_api_key".to_string())
-///     .timeout(30)
+///     .with_timeout(30)
 ///     .build()
 ///     .expect("Failed to build client");
 /// ```
@@ -27,6 +28,27 @@ pub struct DeepSeekClientBuilder {
     api_key: String,
     timeout: Option<u64>,
     host: String,
+}
+
+impl Default for DeepSeekClientBuilder {
+    /// Creates a new `DeepSeekClientBuilder` instance with default settings.
+    ///
+    /// This method attempts to retrieve the API key from the environment variable
+    /// `DEEPSEEK_API_KEY`. If the environment variable is not set, it uses a empty string as
+    /// default API key. The `timeout` is set to `None`, indicating
+    /// no default timeout, and the `host` is set to the default API URL `"https://api.deepseek.com"`.
+    ///
+    /// # Returns
+    /// A new instance of `DeepSeekClientBuilder` with default values.
+    fn default() -> Self {
+        let api_key = env::var("DEEPSEEK_API_KEY").unwrap_or_else(|_| String::from(""));
+
+        DeepSeekClientBuilder {
+            api_key,
+            timeout: None,
+            host: String::from("https://api.deepseek.com"),
+        }
+    }
 }
 
 impl DeepSeekClientBuilder {
@@ -60,10 +82,46 @@ impl DeepSeekClientBuilder {
     /// The `DeepSeekClientBuilder` instance with the timeout configured.
     /// ```ignore
     /// let builder = DeepSeekClientBuilder::new("your_api_key".to_string())
-    ///     .timeout(30);
+    ///     .with_timeout(30);
     /// ```
-    pub fn timeout(mut self, duration: u64) -> Self {
+    pub fn with_timeout(mut self, duration: u64) -> Self {
         self.timeout = Some(duration);
+        self
+    }
+
+    /// Sets the host url for the client.
+    ///
+    /// # Arguments
+    ///
+    /// * `host` - A `string` value representing the host url.
+    ///
+    /// # Returns
+    ///
+    /// The `DeepSeekClientBuilder` instance with the host configured.
+    /// ```ignore
+    /// let builder = DeepSeekClientBuilder::new("your_api_key".to_string())
+    ///     .with_host("https://api.deepseek.com");
+    /// ```
+    pub fn with_host(mut self, host: &str) -> Self {
+        self.host = host.to_string();
+        self
+    }
+
+    /// Sets the api key for the client.
+    ///
+    /// # Arguments
+    ///
+    /// * `api_key` - A `string` value representing the api key.
+    ///
+    /// # Returns
+    ///
+    /// The `DeepSeekClientBuilder` instance with the api key configured.
+    /// ```ignore
+    /// let builder = DeepSeekClientBuilder::new("your_api_key".to_string())
+    ///     .with_api_key("your_api_key");
+    /// ```
+    pub fn with_api_key(mut self, api_key: &str) -> Self {
+        self.api_key = api_key.to_string();
         self
     }
 
@@ -83,7 +141,7 @@ impl DeepSeekClientBuilder {
     ///
     /// ```ignore
     /// let client = DeepSeekClientBuilder::new("your_api_key".to_string())
-    ///     .timeout(30)
+    ///     .with_timeout(30)
     ///     .build()
     ///     .expect("Failed to build client");
     /// ```
@@ -103,5 +161,49 @@ impl DeepSeekClientBuilder {
             client,
             host: self.host,
         })
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use std::env;
+
+    #[test]
+    fn test_deep_seek_client_builder_from_env_var() {
+        env::set_var("DEEPSEEK_API_KEY", "test_api_key");
+
+        let builder = DeepSeekClientBuilder::default().with_timeout(15);
+
+        assert_eq!(builder.host, "https://api.deepseek.com");
+        assert_eq!(builder.timeout, Some(15));
+        assert_eq!(builder.api_key, "test_api_key");
+
+        assert!(builder.build().is_ok());
+    }
+
+    #[test]
+    fn test_deep_seek_client_override_options() {
+        // Build the client using the builder with a provided API key
+        let builder = DeepSeekClientBuilder::new("test_api_key".to_string())
+            .with_host("http://override.com")
+            .with_api_key("another_test_api_keyu");
+
+        assert_eq!(builder.host, "http://override.com");
+        assert_eq!(builder.api_key, "another_test_api_keyu");
+
+        assert!(builder.build().is_ok());
+    }
+
+    #[test]
+    fn test_deep_seek_client_builder_from_new_function() {
+        // Build the client using the builder with a provided API key
+        let builder = DeepSeekClientBuilder::new("test_api_key".to_string()).with_timeout(20);
+
+        assert_eq!(builder.host, "https://api.deepseek.com");
+        assert_eq!(builder.timeout, Some(20));
+        assert_eq!(builder.api_key, "test_api_key");
+
+        assert!(builder.build().is_ok());
     }
 }
